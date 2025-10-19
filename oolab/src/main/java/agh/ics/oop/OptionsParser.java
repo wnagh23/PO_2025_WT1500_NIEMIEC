263437deaa68dfac4a397898bbd63316ea826772
Wojciech <wnagh@student.agh.edu.pl>
2025-10-19 18:53:41 +0200

    Przesłano pliki projektowe dotyczące lab1

---
+++ oolab/src/main/java/agh/ics/oop/OptionsParser.java
@@ -0,0 +1,43 @@
+package agh.ics.oop;
+import agh.ics.oop.model.MoveDirection;
+import java.util.Arrays;
+
+public class OptionsParser {
+
+    public static MoveDirection[] optionsParser(String[] args) {
+
+        MoveDirection[] directions = new MoveDirection[args.length];
+
+        int correctCounter = 0;
+
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            switch (arg) {
+                case "f":
+                    directions[correctCounter] = MoveDirection.FORWARD;
+                    correctCounter++;
+                    break;
+                case "b":
+                    directions[correctCounter] = MoveDirection.BACKWARD;
+                    correctCounter++;
+                    break;
+                case "l":
+                    directions[correctCounter] = MoveDirection.LEFT;
+                    correctCounter++;
+                    break;
+                case "r":
+                    directions[correctCounter] = MoveDirection.RIGHT;
+                    correctCounter++;
+                    break;
+            }
+
+        }
+
+        MoveDirection[] directionsFixed = Arrays.copyOfRange(directions, 0, correctCounter);
+
+        return directionsFixed;
+
+    }
+
+}
+
--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,33 +1,33 @@
 package agh.ics.oop;
 
-import org.w3c.dom.ls.LSOutput;
-
-import java.util.Arrays;
+import agh.ics.oop.model.MoveDirection;
 
 public class World {
 
     static void main(String[] args) {
-        System.out.println("system wystartowal");
-        for (int i = 0; i < args.length; i++) {
-            switch (args[i]) {
-                case "f":
+
+        System.out.println("System wystartowal");
+
+        MoveDirection[] directions = OptionsParser.optionsParser(args);
+
+        for (MoveDirection direction : directions) {
+            switch (direction) {
+                case FORWARD:
                     System.out.println("Zwierzak idzie do przodu");
                     break;
-                case "b":
+                case BACKWARD:
                     System.out.println("Zwierzak idzie do tylu");
                     break;
-                case "l":
+                case LEFT:
                     System.out.println("Zwierzak idzie w lewo");
                     break;
-                case "r":
+                case RIGHT:
                     System.out.println("Zwierzak idzie w prawo");
                     break;
-                    default:
-                        System.out.println("Prosze podac prawidlowy kierunek");
             }
 
         };
-        System.out.println("system zakonczyl dzialanie");
+        System.out.println("System zakonczyl dzialanie");
     }
 
 
